963f2f28f8934c3b8a6ee259bcc3b79511f3e68d
daniel <daniel.walaker@gmail.com>
2025-04-14 01:28:31 +0200

    Updated application.properties file to be able to connect to local database

--- src/main/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplication.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VelocitymediaBackendApplication {
+public class VelocitymediaBackendApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(VelocitymediaBackendApplication.class, args);
 	}
+
 
 }
