902af290b93d5b4306a1f86b7eb0acbc53505352
daniel <daniel.walaker@gmail.com>
2025-04-13 02:49:37 +0200

    Spring boot project initialized

---
+++ src/main/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplication.java
@@ -0,0 +1,13 @@
+package no.velocitymedia.velocitymedia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocitymediaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelocitymediaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.velocitymedia.velocitymedia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelocitymediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
