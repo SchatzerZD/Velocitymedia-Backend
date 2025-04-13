91fe83dcc8f7d810605c6b87a2637a2836c56ef6
daniel <daniel.walaker@gmail.com>
2025-04-14 01:50:50 +0200

    Created a video repository

---
+++ src/main/java/no/velocitymedia/velocitymedia_backend/repository/VideoRepository.java
@@ -0,0 +1,16 @@
+package no.velocitymedia.velocitymedia_backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import no.velocitymedia.velocitymedia_backend.model.VideoEntity;
+
+
+public interface VideoRepository extends JpaRepository<VideoEntity, Long>{
+
+
+    List<VideoEntity> getAllVideos();
+
+
+}
