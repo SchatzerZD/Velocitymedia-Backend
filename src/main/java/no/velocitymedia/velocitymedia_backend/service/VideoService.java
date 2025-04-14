4cf991448809adb92bee67781c65067df43fce03
daniel <daniel.walaker@gmail.com>
2025-04-14 03:25:31 +0200

    Added method for adding video to repository

--- src/main/java/no/velocitymedia/velocitymedia_backend/service/VideoService.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/service/VideoService.java
@@ -3,10 +3,14 @@ package no.velocitymedia.velocitymedia_backend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import no.velocitymedia.velocitymedia_backend.model.VideoEntity;
 import no.velocitymedia.velocitymedia_backend.repository.VideoRepository;
 
+@Service
+@Transactional
 public class VideoService {
 
     @Autowired
@@ -14,6 +18,15 @@ public class VideoService {
 
     public List<VideoEntity> findAll() {
         return videoRepository.findAll();
+    }
+
+
+    public void addVideo(String videoName, String filePath){
+        VideoEntity newVideo = new VideoEntity();
+        newVideo.setVideoName(videoName);
+        newVideo.setFilePath(filePath);
+
+        videoRepository.save(newVideo);
     }
 
 
