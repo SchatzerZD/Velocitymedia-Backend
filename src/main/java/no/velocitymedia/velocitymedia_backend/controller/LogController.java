5a4fcd92d30bcaf8b90d44abaada0a08192f35b9
daniel <daniel.walaker@gmail.com>
2025-04-14 20:41:01 +0200

    Minor refactorings and bug fixes

--- src/main/java/no/velocitymedia/velocitymedia_backend/controller/LogController.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/controller/LogController.java
@@ -2,7 +2,6 @@ package no.velocitymedia.velocitymedia_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -41,6 +39,12 @@ public class LogController {
         UserEntity targetUser = userService.getUserById(Long.parseLong(userId));
         if(targetUser == null){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("User not found");
+        }
+
+        try {
+            logEntity.getLog().equals(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Log cannot be null");
         }
 
         logEntity.setUserEntity(targetUser);
