eb24a887a022eebc87d1756b0d350079a9a1aab4
daniel <daniel.walaker@gmail.com>
2025-04-14 03:24:38 +0200

    Added mapping for post request to upload video

--- src/main/java/no/velocitymedia/velocitymedia_backend/controller/VideoController.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/controller/VideoController.java
@@ -3,12 +3,19 @@ package no.velocitymedia.velocitymedia_backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import no.velocitymedia.velocitymedia_backend.model.VideoEntity;
 import no.velocitymedia.velocitymedia_backend.service.VideoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RequestMapping(value = "/video")
@@ -20,9 +27,21 @@ public class VideoController {
 
 
     @GetMapping("/")
-    public List<VideoEntity> getVideos() {
-        return videoService.findAll();
+    public ResponseEntity<List<VideoEntity>> getVideos() {
+        return ResponseEntity.ok(videoService.findAll());
     }
+
+    @PostMapping("/")
+    public ResponseEntity<?> uploadVideo(@RequestParam String videoName, @RequestParam String videoFilePath) {
+        try {
+            videoService.addVideo(videoName,videoFilePath);
+            return ResponseEntity.ok().body("Video added");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+    }
+    
     
 
 }
