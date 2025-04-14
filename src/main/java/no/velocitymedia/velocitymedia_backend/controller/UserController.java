65845f84062e9124ef0f3bb960a4a9b0a56bcbac
daniel <daniel.walaker@gmail.com>
2025-04-14 17:40:45 +0200

    Added temporary admin security check for fetching all users in database

--- src/main/java/no/velocitymedia/velocitymedia_backend/controller/UserController.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +30,12 @@ public class UserController {
     private JWTService jwtService;
 
     @GetMapping("/")
-    public ResponseEntity<List<UserEntity>> getUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+    public ResponseEntity<?> getUsers(@AuthenticationPrincipal UserEntity user) {
+        if(user.getUsername().equals("admin")){
+            return ResponseEntity.ok(userService.getAllUsers());
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+        }
     }
 
     @PostMapping("/")
