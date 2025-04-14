0e5015a785151365ebbf17333ce03cf5578d0c60
daniel <daniel.walaker@gmail.com>
2025-04-14 05:55:20 +0200

    Added security filtering with jwt

--- src/main/java/no/velocitymedia/velocitymedia_backend/config/JWTRequestFilter.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/config/JWTRequestFilter.java
@@ -1,16 +1,25 @@
 package no.velocitymedia.velocitymedia_backend.config;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import no.velocitymedia.velocitymedia_backend.model.UserEntity;
+import no.velocitymedia.velocitymedia_backend.repository.UserRepository;
 import no.velocitymedia.velocitymedia_backend.service.JWTService;
+import no.velocitymedia.velocitymedia_backend.service.UserService;
 
 @Component
 public class JWTRequestFilter extends OncePerRequestFilter{
@@ -19,6 +28,9 @@ public class JWTRequestFilter extends OncePerRequestFilter{
     @Autowired
     private JWTService jwtService;
 
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException 
@@ -26,8 +38,17 @@ public class JWTRequestFilter extends OncePerRequestFilter{
         String tokenHeader = request.getHeader("Authorization");
         if(tokenHeader != null && tokenHeader.startsWith("Bearer ")){
             String token = tokenHeader.substring(7);
+            try {
+                String username = jwtService.getUsername(token);
+                UserEntity user = userService.getUserByName(username);
 
-            
+                if(user != null){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (JWTDecodeException ignored) {
+            }
         }
 
         filterChain.doFilter(request, response);
