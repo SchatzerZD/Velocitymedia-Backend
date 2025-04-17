94385b02753b92a8d07257d1876af7004bf671cb
daniel <daniel.walaker@gmail.com>
2025-04-17 08:52:28 +0200

    Updated log size in database

--- src/main/java/no/velocitymedia/velocitymedia_backend/model/LogEntity.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/model/LogEntity.java
@@ -1,5 +1,6 @@
 package no.velocitymedia.velocitymedia_backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -21,6 +22,7 @@ public class LogEntity {
     @JoinColumn(name = "user_id")
     private UserEntity userEntity;
 
+    @Column(length = 4000)
     private String log;
 
     public LogEntity() {
