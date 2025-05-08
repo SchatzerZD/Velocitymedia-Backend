84d17321e85f5c985ce90ea1eb2d8619a3d83cc2
daniel <daniel.walaker@gmail.com>
2025-05-08 14:07:35 +0200

    Added command line runner for always adding admin user to database

--- src/main/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplication.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/VelocitymediaBackendApplication.java
@@ -1,7 +1,13 @@
 package no.velocitymedia.velocitymedia_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import no.velocitymedia.velocitymedia_backend.model.UserEntity;
+import no.velocitymedia.velocitymedia_backend.service.UserService;
 
 @SpringBootApplication
 public class VelocitymediaBackendApplication{
@@ -9,6 +15,25 @@ public class VelocitymediaBackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VelocitymediaBackendApplication.class, args);
+	}
+
+	@Component
+	public class CommandLineAppStartupRunner implements CommandLineRunner{
+		@Autowired
+		UserService userService;
+
+		@Override
+		public void run(String... args) throws Exception {
+			try {
+				UserEntity admin = new UserEntity();
+				admin.setUsername("admin");
+				admin.setPassword("admin");
+				userService.addUser(admin);
+			} catch (IllegalArgumentException e) {
+				System.out.println("Admin already added");
+			}
+		}
+		
 	}
 
 
