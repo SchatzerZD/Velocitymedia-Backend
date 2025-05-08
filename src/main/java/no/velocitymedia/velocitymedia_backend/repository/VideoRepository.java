1fc15ccbadede5ba38c5febd53a1f82adeca0fdc
daniel <daniel.walaker@gmail.com>
2025-05-08 14:04:59 +0200

    Refactored to using projects instead of users, and added flag functionality

--- src/main/java/no/velocitymedia/velocitymedia_backend/repository/VideoRepository.java
+++ src/main/java/no/velocitymedia/velocitymedia_backend/repository/VideoRepository.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import no.velocitymedia.velocitymedia_backend.model.UserEntity;
+import no.velocitymedia.velocitymedia_backend.model.ProjectEntity;
 import no.velocitymedia.velocitymedia_backend.model.VideoEntity;
+import no.velocitymedia.velocitymedia_backend.model.VideoFlag;
 
 
 public interface VideoRepository extends JpaRepository<VideoEntity, Long>{
 
-    List<VideoEntity> findByUser(UserEntity user);
+    List<VideoEntity> findByProject(ProjectEntity project);
+    List<VideoEntity> findByProjectAndVideoFlag(ProjectEntity project, VideoFlag videoFlag);
 
 
 }
